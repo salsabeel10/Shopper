d5402775a8c1e9496a76c23c4ba55b9232dca4c2
salsabeel10 <salsabeelibrahim327@gmail.com>
2023-12-21 13:25:22 +0400

    shop page completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import '../Navbar/Navbar.css'
 
 import logo from '../Assets/logo.png'
 import card_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
 
@@ -16,14 +17,14 @@ export const Navbar = () => {
             <p>Shopper</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>} </li>
-            <li onClick={()=>{setMenu("mens")}}>Men {menu==="mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}>Women {menu==="womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}>Kids {menu==="kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}> <Link to='/'>Shop</Link> {menu==="shop"?<hr/>:<></>} </li>
+            <li onClick={()=>{setMenu("mens")}}> <Link to='/mens'>Men</Link> {menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}> <Link to='/womens'>Women</Link> {menu==="womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}> <Link to='/kids'>Kids</Link> {menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={card_icon} alt="cart_icon" />
+            <Link to='/login'><button>Login</button></Link>
+            <Link to='cart'><img src={card_icon} alt="cart_icon" /></Link>
             <div className="nav-cart-count">
                 0
             </div>
